45b28313cc6ad7def598423b528ede827fb5884f
Thilak-Goovo <Thilak.G@goovo.in>
2026-04-03 15:13:32 +0530

    fixed API URL10

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import { useEffect } from "react";
 import axios from "axios";
 import Home from "./pages/Home";
-const API_URL = process.env.REACT_APP_API_URL;
+// const API_URL = process.env.REACT_APP_API_URL;
 function App() {
 
-  // useEffect(() => {
-  //   // axios.get("http://localhost:5001")
-  //   axios.get(`${API_URL}/api/diagnose`, data)
+  //   // useEffect(() => {
+  //   //   // axios.get("http://localhost:5001")
+  //   //   axios.get(`${API_URL}/api/diagnose`, data)
+  //   //     .then(res => console.log(res.data))
+  //   //     .catch(err => console.log(err));
+  //   // }, []);
+  //   // axios.post(`${API_URL}/api/diagnose`, data);
+  //   axios.post(`${API_URL}/api/diagnose`, data)
   //     .then(res => console.log(res.data))
   //     .catch(err => console.log(err));
-  // }, []);
-  // axios.post(`${API_URL}/api/diagnose`, data);
-  axios.post(`${API_URL}/api/diagnose`, data)
-    .then(res => console.log(res.data))
-    .catch(err => console.log(err));
   return (
     <Home />
   );
